1ba28ed18f88f514aab3f986c9ae3b12c873b50d
Karim DRIDI <karim.dridi@gmail.com>
2016-02-14 17:20:03 +0100

    updating actraci

--- atraci/launch.sh
+++ atraci/launch.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 ./build.sh
 
 source config
-docker run --rm --net=host -v /tmp/.X11-unix:/tmp/.X11-unix $CONFIG_APP_NAME
+docker run --rm -it --net=host -v /tmp/.X11-unix:/tmp/.X11-unix $CONFIG_APP_NAME $1
