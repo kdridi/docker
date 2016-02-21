318557c5015d4e685adf67e06f523f7094e6bb15
Karim DRIDI <karim.dridi@gmail.com>
2016-02-21 19:16:28 +0100

    updating configuration

--- nasm/launch.sh
+++ nasm/launch.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 ./build.sh
 
 source config
-docker run --rm -it --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/app:/app $CONFIG_APP_NAME
+docker run --rm -it --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/app:/app $CONFIG_APP_NAME $1
